bae1b3cf206c76495d3821206b90db8f7344c79a
student <student@Achievers-iMac.local>
2017-08-30 11:22:30 +0530

    Initial Commit

---
+++ layoutsauto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  layoutsauto
+//
+//  Created by student on 8/30/17.
+//  Copyright © 2017 Panoptic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ layoutsauto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  layoutsauto
+//
+//  Created by student on 8/30/17.
+//  Copyright © 2017 Panoptic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
